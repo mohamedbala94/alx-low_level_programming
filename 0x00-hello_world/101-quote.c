1ca8d336475a5092236bc38f777815f655f7f546
mohamedbala94 <wg3it.94@gmail.com>
2023-02-17 07:16:35 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 
 /**
  * main - Entry point
- * Return: Always 1 (Success)
+ * Return 1 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
