5c3157ea521b92f665b4afdd75c666dd358bb46c
mohamedbala94 <wg3it.94@gmail.com>
2023-02-18 03:44:41 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
-
+#include <unistd.h>
 /**
- * main - Entry point
- * Return 1 (Success)
+ * main - C program that print statment with new line to standard error.
+ *
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
